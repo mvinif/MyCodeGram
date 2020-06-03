54a0835166c0833480df2d94e52598566c618c53
Marcos Vinicius F. Machado <mvfmachado.contato@gmail.com>
2020-06-03 10:06:09 -0300

    follow button

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -10,7 +10,7 @@ class FollowsController extends Controller
 
     public function store(User $user)
     {
-        return $user->username;
+        return auth()->user()->following()->toggle($user->profile);
     }
 
 }
